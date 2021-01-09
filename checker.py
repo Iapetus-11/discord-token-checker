da06f245edfb455fb36d416696e15d1e3cc7b79e
Milo Weinberg <iapetus011@gmail.com>
2021-01-09 12:51:46 -0500

    remove lambda in checker.py

--- checker.py
+++ checker.py
@@ -1,7 +1,7 @@
 import multiprocessing as mp
 import requests as rs
 
-check = (lambda t: rs.get('https://discordapp.com/api/v6/users/@me/library', headers={'authorization': t}).status_code == 200)
+def check(t): rs.get('https://discordapp.com/api/v6/users/@me/library', headers={'authorization': t}).status_code == 200
 
 if __name__ == '__main__':
     with mp.Pool(20) as p:
